046c0f0abcaeabf3902c164d201b1b3a69fd0d6f
Patrik Arell <patrikarell@gmail.com>
2024-06-19 15:01:53 +0200

    button didnt have a link

--- src/components/ShowAnimalsHome.tsx
+++ src/components/ShowAnimalsHome.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { IAnimal } from "../models/IAnimal"
 import { CustomCursor } from "./CustomCursor"
 import { ShowAnimal } from "./ShowAnimal"
+import { Link } from "react-router-dom"
 
 interface IShowAnimalsHomeProps {
     animalsInState: IAnimal[]
@@ -14,7 +15,8 @@ export const ShowAnimalsHome = ({ animalsInState }: IShowAnimalsHomeProps) => {
     return (<>
         {!animalsInState.every(isFed) && <h2>Alla hungriga djur</h2>}
         {animalsInState.every(isFed) && <><h2>Alla djur är för tillfället mätta!</h2>
-            <button className="hero-cta">Läs mer om djuren</button></>}
+        
+            <Link to={'/animals'}><button className="hero-cta">Läs mer om djuren</button></Link></>}
         <ul className="animal__list--home" onMouseEnter={() => setCursorActive(true)} onMouseLeave={() => setCursorActive(false)}>
             {animalsInState?.map((animal) => (
                 !animal.isFed ?
