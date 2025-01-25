866e5571f9128c41cf588eee20c16534ce236d0e
Patrik Arell <patrikarell@gmail.com>
2025-01-25 23:00:09 +0100

    refactor: improve type safety by using EventListener type in useWindowListener hook

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -68,7 +68,7 @@ export const CustomCursor = () => {
     }
 
 
-    useWindowListener('pointermove', (moveCursor));
+    useWindowListener('pointermove', (moveCursor as EventListener));
 
     return (
         <>
--- src/hooks/useWindowListener.ts
+++ src/hooks/useWindowListener.ts
@@ -1,10 +1,6 @@
 import { useEffect } from 'react';
 
-interface WindowListener {
-  (event: Event): void;
-}
-
-export function useWindowListener(eventType: string, listener: WindowListener): void {
+export function useWindowListener(eventType: string, listener: EventListener): void {
   useEffect(() => {
     window.addEventListener(eventType, listener);
     return () => {
