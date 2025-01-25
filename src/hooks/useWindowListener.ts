782c30ba575ec6f3eba7b3f7492a6a11c66eed24
Patrik Arell <patrikarell@gmail.com>
2025-01-25 22:52:17 +0100

    refactor: enhance type safety by defining a WindowListener interface for useWindowListener hook

--- src/hooks/useWindowListener.ts
+++ src/hooks/useWindowListener.ts
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 
-export function useWindowListener(eventType: string, listener: EventListenerOrEventListenerObject) {
+interface WindowListener {
+  (event: Event): void;
+}
+
+export function useWindowListener(eventType: string, listener: WindowListener): void {
   useEffect(() => {
     window.addEventListener(eventType, listener);
     return () => {
